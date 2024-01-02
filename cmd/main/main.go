95ce30e33d28fb223a0dfabea4474e1349175ad4
andrey.savelichev <kek@kek>
2024-01-02 13:41:14 +0300

    feature: add-conf-file -add confing parser changelog: feature

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"L0/internal/config"
+	"log"
+	"os"
+)
 
+func main() {
+	config, err := config.LoadConfig(os.Getenv("CONF_PATH"))
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
 }
